af2c23f397e58e1f6c924911a5654e2444ea18b2
Vivek Agarwal <vivekvk0812@gmail.com>
2026-08-14 18:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.54%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
+long long fun(long long base,long long poww ){
+    const long long MOD=1000000007;
+    if(poww==0){
+return 1;}
 
-long long power(long long base , long long p){
-    const long long MOD = 1000000007;
-    if(p==0){return 1;}
-    else if(base==1 ){return 1;}  
-    long long ans=1;
-    while(p>0){
-    if(p%2==0){
-        p=p/2;
-        base=(base*base)%MOD;
-
-    }
-    else if(p%2!=0){
-        ans=(ans*base)%MOD;
-        p=p-1;
-    }}
-return ans;
+if(poww%2==0){
+   return fun(base*base%MOD,poww/2);
 }
+else{
+    return base * fun(base,poww-1)%MOD;
+}    
+    
+    }
+
+
+
     int countGoodNumbers(long long n) {
+        const long long MOD=1000000007;
         long long even=1;
-        long long odd=1;
-        if(n%2==0){
-            even=n/2;
-            odd=n/2;
-        }
-        else{
-            even=(n/2)+1;
-            odd=n/2;
-        }
-        even=power(5,even);
-        odd=power(4,odd);
-        return (odd*even)% 1000000007;
+        long long odd = 1;
+        if(n%2==0){even=n/2;odd=n/2;}
+        else{even=(n/2)+1;odd=n/2;}
+        even=fun(5,even);
+        odd=fun(4,odd);
+        return (even*odd)%MOD;
     }
 };
