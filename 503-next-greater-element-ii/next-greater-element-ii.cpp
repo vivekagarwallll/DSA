c0e0d8092a7f7dcf2f11199b3ebe2a908ee963b3
Vivek Agarwal <vivekvk0812@gmail.com>
2026-09-08 14:24:58 +0530

    Time: 102 ms (6.40%) | Memory: 27.9 MB (97.74%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n=arr.size();
                 vector<int>ans(n,-1);
         for(int i =0;i<n;i++){
-            for(int j=i+1;j<=i+n-1;j++){
+            for(int j=i+1;j<i+n;j++){
                 int idx= j%n;
                 if(arr[idx]>arr[i]){
                     ans[i]=arr[idx];
