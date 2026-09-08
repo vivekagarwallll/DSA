50d8905e3bf0c6b0f6d7a06c0f03bfc3835ee2c9
Vivek Agarwal <vivekvk0812@gmail.com>
2026-09-09 02:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (93.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
-vector<int> fun1(vector<int>& arr){
-           int n=arr.size();
-    vector<int>premax(n);
-premax[0]=arr[0];
-        for(int i=1;i<n;i++){
-if(arr[i]>premax[i-1]){
-    premax[i]=arr[i];
-}
-else{
-    premax[i]=premax[i-1];
-}
-}
-return premax;
-}
-
-vector<int> fun2(vector<int>& arr){
-int n=arr.size();
-    vector<int>submax(n);
-submax[n-1]=arr[n-1];
-for(int i=n-2;i>=0;i--){
-    if(arr[i]>submax[i+1]){
-        submax[i]=arr[i];
-    }
-    else{
-       submax[i]=submax[i+1]; 
-    }
-}
-return submax;
-}
-
-
-    int trap(vector<int>& height) {
-        int n=height.size();
+    int trap(vector<int>& arr) {
+        int n=arr.size();
+        int leftmax=0;
+        int rightmax=0;
         int total=0;
-vector<int>premax = fun1(height);
-vector<int>submax = fun2(height);
-
-        for(int i=0;i<n;i++){
-            int leftmax=premax[i];
-            int rightmax=submax[i];
-            if(height[i]<leftmax &&height[i]<rightmax ){
-total=total+min(leftmax,rightmax)-height[i];
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            leftmax=max(arr[l],leftmax);
+            rightmax=max(arr[r],rightmax);
+            if(arr[l]<=arr[r]){
+                if(arr[l]<leftmax){
+                    total=total+(leftmax-arr[l]);
+                }
+                l++;
+            }
+            else if(arr[l]>arr[r]){
+                if(arr[r]<rightmax){
+                    total=total+(rightmax-arr[r]);
+                }
+                r--;
             }
         }
-        return total;
+return total;
     }
 };
